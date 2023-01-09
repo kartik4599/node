0713ef2eebd106d74541f3a2b1af61360c15495b
devloper-kartik <kartikmendu2@gmail.com>
2023-01-09 13:29:45 +0530

    map

--- first.js
+++ first.js
@@ -1,3 +1,25 @@
-const multiply = (a, b) => a * b;
+// const fruits = ["apple", "oranges", "", "mango", "", "lemon"];
 
-console.log(multiply(10,5));
+// const transformArray = fruits.map((e, i) => {
+//   if (e.length===0) {
+//     return "empty string";
+//   }
+//   return e;
+// });
+// console.log(transformArray);
+
+const obj1 = {'key1': 1}
+
+const obj2 = { ...obj1}
+
+if(obj2 === obj1){
+
+console.log('same objects')
+
+}
+
+else{
+
+console.log('different objects')
+
+}
