bd5dcc18dcefedc5d0504bb5931cf08dde689c8d
devloper-kartik <kartikmendu2@gmail.com>
2023-01-11 14:46:06 +0530

    get data

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const http = require("http");
 
+let show = "";
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/home") {
-    res.write(`<html><body><h1>Welcome home</h1><body><html>`);
+  console.log(req.method);
+  if (req.url === "/" && req.method === "POST") {
+    const collect = [];
+    req.on("data", (recived) => {
+      collect.push(recived);
+    });
+    req.on("end", () => {
+      const display = Buffer.concat(collect).toString();
+      console.log(display);
+      show = display.split("=")[1];
+    });
   }
-  if (req.url === "/about") {
-    res.write(`<html><body><h1>About us page</h1><body><html>`);
-  }
-  if (req.url === "/node") {
-    res.write(`<html><body><h1>Welcome to node project </h1><body><html>`);
-  }
-  if (req.url === "/") {
-    res.write(
-      `<html><body>
-      <h3><a href="/home">Go to home</a> </h3>
-      <h3><a href="/about">Go to about page</a> </h3>
-      <h3><a href="/node">Go to node page</a> </h3>
-      <body><html>`
-    );
-  }
-  // res.write(`<html><body><h1>Hii</h1><body><html>`);
+
+  res.write(
+    `<html><body>
+    <form action="/" method="POST"> 
+    <h3>${show}<h3>
+      <input name="message" /> 
+      <button type="submit">Send</button>
+    </form>
+    <body><html>`
+  );
+  res.end();
 });
 
 server.listen(4000);
