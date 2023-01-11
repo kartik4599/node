4a6e264487b171f62706032a2ad4fe5268bfb054
devloper-kartik <kartikmendu2@gmail.com>
2023-01-11 15:28:05 +0530

    route.js

---
+++ routes.js
@@ -0,0 +1,29 @@
+let show = "";
+
+const route = (req, res) => {
+  console.log(req.method);
+  if (req.url === "/" && req.method === "POST") {
+    const collect = [];
+    req.on("data", (recived) => {
+      collect.push(recived);
+    });
+    req.on("end", () => {
+      const display = Buffer.concat(collect).toString();
+      console.log(display);
+      show = display.split("=")[1];
+    });
+  }
+
+  res.write(
+    `<html><body>
+      <form action="/" method="POST"> 
+      <h3>${show}<h3>
+        <input name="message" /> 
+        <button type="submit">Send</button>
+      </form>
+      <body><html>`
+  );
+  res.end();
+};
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,31 +1,6 @@
 const http = require("http");
+const routes = require("./routes");
 
-let show = "";
-
-const server = http.createServer((req, res) => {
-  console.log(req.method);
-  if (req.url === "/" && req.method === "POST") {
-    const collect = [];
-    req.on("data", (recived) => {
-      collect.push(recived);
-    });
-    req.on("end", () => {
-      const display = Buffer.concat(collect).toString();
-      console.log(display);
-      show = display.split("=")[1];
-    });
-  }
-
-  res.write(
-    `<html><body>
-    <form action="/" method="POST"> 
-    <h3>${show}<h3>
-      <input name="message" /> 
-      <button type="submit">Send</button>
-    </form>
-    <body><html>`
-  );
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(4000);
