9c5cd2bf730831cac6e0a7ff20e5e14e3bc6b8c2
devloper-kartik <kartikmendu2@gmail.com>
2023-01-12 13:44:21 +0530

    multiple routes

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const http = require("http");
-const express = require("express");
+const app = require("express")();
+const bodyParser = require("body-parser");
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  next();
+app.use("/add-product", (req, res, next) => {
+  res.send(`
+  <form action="/product" method="POST"> 
+    <lable> Product Name</lable>
+    <input name="name" />
+    <br/>
+    <lable>Quantity</lable>
+    <input name="size" />
+    <br/>
+    <button type="submit">Add</button>
+  </form>
+  `);
 });
 
-app.use((req, res, next) => {
-  res.send("<h1>This is Express Project");
+app.use("/product", (req, res, next) => {
+    console.log("Name => "+ req.body.name);
+    console.log("Quantity => "+ req.body.name);
+    res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to Project</h1>");
 });
 
 app.listen(4000);
